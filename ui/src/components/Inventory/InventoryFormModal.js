4e58bb2f098b5ca3b79ce68688e28d840092c02c
KennyMcD <kennymcdonnell1997@gmail.com>
2024-01-27 16:48:54 -0500

    Adding new fields

---
+++ ui/src/components/Inventory/InventoryFormModal.js
@@ -0,0 +1,125 @@
+import Button from '@material-ui/core/Button'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import Grid from '@material-ui/core/Grid'
+import { MeasurementUnits } from '../../constants/units/'
+import React from 'react'
+import TextField from '../Form/TextField'
+import { Field, Form, Formik } from 'formik'
+
+class InventoryFormModal extends React.Component {
+  render() {
+    const {
+      formName,
+      handleDialog,
+      handleInventory,
+      title,
+      initialValues,
+      products
+    } = this.props
+    return (
+      <Dialog
+        open={this.props.isDialogOpen}
+        maxWidth='sm'
+        fullWidth={true}
+        onClose={() => { handleDialog(false) }}
+      >
+        <Formik
+          initialValues={initialValues}
+          onSubmit={values => {
+            handleInventory(values)
+            handleDialog(true)
+          }}>
+          {helpers =>
+            <Form
+              noValidate
+              autoComplete='off'
+              id={formName}
+            >
+              <DialogTitle id='alert-dialog-title'>
+                {`${title} Inventory`}
+              </DialogTitle>
+              <DialogContent>
+                <Grid container>
+                  <Grid item xs={12} sm={12}>
+                    <Field
+                      custom={{ variant: 'outlined', fullWidth: true, }}
+                      name='name'
+                      label='Name'
+                      component={TextField}
+                    />
+                    <Field
+                      custom={{ variant: 'outlined', fullWidth: true, }}
+                      name='productType'
+                      label='Product Type'
+                      component="select"
+                    >
+                      <option productType=''></option>
+                      {products.map(product => <option productType={product.name}>{product.name}</option> )}
+                    </Field>
+                    <Field
+                      custom={{ variant: 'outlined', fullWidth: true, }}
+                      name='description'
+                      label='Description'
+                      component={TextField}
+                      defaultValue=''
+                    />
+                    <Field
+                      custom={{ variant: 'outlined', fullWidth: true, }}
+                      name='averagePrice'
+                      label='Average Price'
+                      component={TextField}
+                      defaultValue='0'
+                    />
+                    <Field
+                      custom={{ variant: 'outlined', fullWidth: true, }}
+                      name='amount'
+                      label='Amount'
+                      component={TextField}
+                      defaultValue='0'
+                    />
+                    <Field
+                      custom={{ variant: 'outlined', fullWidth: true, }}
+                      name='unitOfMeasurement'
+                      label='Unit Of Measurement'
+                      component="select"
+                    >
+                      <option>{MeasurementUnits.CUP.name}</option>
+                      <option>{MeasurementUnits.GALLON.name}</option>
+                      <option>{MeasurementUnits.OUNCE.name}</option>
+                      <option>{MeasurementUnits.PINT.name}</option>
+                      <option>{MeasurementUnits.POUND.name}</option>
+                      <option>{MeasurementUnits.QUART.name}</option>
+                    </Field>
+                    <Field
+                      custom={{ variant: 'outlined', fullWidth: true, }}
+                      name='bestBeforeDate'
+                      label='Best Before Date'
+                      component={TextField}
+                    />
+                  </Grid>
+                </Grid>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={() => { handleDialog(false) }} color='secondary'>Cancel</Button>
+                <Button
+                  disableElevation
+                  variant='contained'
+                  type='submit'
+                  form={formName}
+                  color='secondary'
+                  disabled={!helpers.dirty}>
+                  Save
+                </Button>
+              </DialogActions>
+            </Form>
+          }
+        </Formik>
+      </Dialog>
+    )
+  }
+}
+
+export default InventoryFormModal
